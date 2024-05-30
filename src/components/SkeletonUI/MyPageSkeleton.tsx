c79b2ae85b12f29ce084bc048946e2ec3a7ec97c
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2024-05-30 16:25:48 +0900

    Add: MyPageSkeleton component

---
+++ src/components/SkeletonUI/MyPageSkeleton.tsx
@@ -0,0 +1,26 @@
+import { UserSkeleton } from './UserSkeleton';
+import { Section } from '../Section';
+
+function MyPageSkeleton() {
+  return (
+    <section id='my-page-container' className='flex flex-col items-center'>
+      <Section.Container className='bg-white'>
+        <UserSkeleton.Profile />
+        <div className='mt-4'>
+          <UserSkeleton.Pet />
+        </div>
+        <div>
+          <UserSkeleton.Activity />
+          <div className='flex flex-col gap-y-4 mt-4'>
+            <div className='w-full h-9 rounded-[10px] bg-bg-blue' />
+            <div className='w-full h-9 rounded-[10px] bg-bg-blue' />
+            <div className='w-full h-9 rounded-[10px] bg-bg-blue' />
+            <div className='w-full h-9 rounded-[10px] bg-bg-blue' />
+          </div>
+        </div>
+      </Section.Container>
+    </section>
+  );
+}
+
+export default MyPageSkeleton;
