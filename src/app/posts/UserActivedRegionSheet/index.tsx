4e4e054d25036e493420f3f1242f65833c7fb4bd
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2024-07-03 17:55:02 +0900

    Fix: UserActivedRegionSheet 오류 개선

--- src/app/posts/UserActivedRegionSheet/index.tsx
+++ src/app/posts/UserActivedRegionSheet/index.tsx
@@ -16,16 +16,15 @@ interface UserActivedRegionSheetProps {
 function UserActivedRegionSheet({ isOpen, regions, activedRegion, onClick, onClose }: UserActivedRegionSheetProps) {
   return (
     <TopSheet isOpen={isOpen} onClose={onClose} className='top-14 text-left'>
-      {activedRegion &&
-        regions.map(({ id, region }) => (
-          <SheetButton
-            key={id}
-            onClick={() => onClick(id)}
-            className={`px-4 text-left ${activedRegion.region_id === id ? 'text-main-1' : 'text-text-2 '}`}
-          >
-            {formatRegionTitle(region)}
-          </SheetButton>
-        ))}
+      {regions.map(({ id, region }) => (
+        <SheetButton
+          key={id}
+          onClick={() => onClick(id)}
+          className={`px-4 text-left ${activedRegion?.region_id === id ? 'text-main-1' : 'text-text-2 '}`}
+        >
+          {formatRegionTitle(region)}
+        </SheetButton>
+      ))}
       <NativeLink href='/user/region' className='py-[10px] px-4 text-text-2 border-b-[1px]'>
         동네 설정
       </NativeLink>
