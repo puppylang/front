481b4055560ab4e2552b399b412460bd3a4e7166
hyebin <sksp0401@gmail.com>
2024-07-03 21:35:32 +0900

    Fix: 토큰 로직 수정

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,29 +1,18 @@
 export const getToken = () => {
   let token = '';
-  if (isWebview()) {
-    token = localStorage.getItem('token') || '';
-  }
-
-  if (!isWebview()) {
-    const cookie = document.cookie.split('; ').find(row => row.startsWith('token'));
-    token = cookie ? cookie.split('=')[1] : '';
-  }
+  const cookie = document.cookie.split('; ').find(row => row.startsWith('token'));
+  token = cookie ? cookie.split('=')[1] : '';
 
   return token;
 };
 
 export const saveToken = (token: string) => {
-  if (isWebview()) {
-    localStorage.setItem('token', token);
+  const cookie = document.cookie.split('; ').find(row => row.startsWith('token'));
+
+  if (cookie) {
+    deleteCookie();
   }
 
-  if (!isWebview()) {
-    const cookie = document.cookie.split('; ').find(row => row.startsWith('token'));
-
-    if (cookie) {
-      deleteCookie();
-    }
-  }
   document.cookie =
     process.env.NODE_ENV === 'development' ? `token=${token};path=/;` : `token=${token};path=/; SameSite=None; Secure;`;
 };
