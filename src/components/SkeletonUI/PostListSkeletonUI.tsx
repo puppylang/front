2a8e74373433b359d42b5ad677b604d21546f223
이강윤 <rkddbs1031@gmail.com>
2024-07-19 18:29:29 +0900

    Fix: PostListSkeletonUI 모듈화

--- src/components/SkeletonUI/PostListSkeletonUI.tsx
+++ src/components/SkeletonUI/PostListSkeletonUI.tsx
@@ -1,15 +1,42 @@
-const DivLength = 3;
+import { UserSkeleton } from './UserSkeleton';
+import { Section } from '../Section';
 
-function PostListSkeletonUI() {
+function PostContainerSkeletonUI({ bgColor = 'bg-white' }: { bgColor?: string }) {
+  return (
+    <div className='flex flex-col items-center'>
+      <Section.Container>
+        <UserSkeleton.Pet petBGColor={bgColor || ''} />
+
+        <div className='mt-4'>
+          <div className='flex justify-between items-center animate-pulse'>
+            <div className={`animate-pulse w-[150px] h-4 rounded-[10px] mb-4 ${bgColor || ''}`} />
+            <div className={`animate-pulse w-[100px] h-8 rounded-[10px] mb-4 ${bgColor || ''}`} />
+          </div>
+          <PostListSkeletonUI length={3} bgColor={bgColor || ''} />
+        </div>
+      </Section.Container>
+    </div>
+  );
+}
+
+function PostListSkeletonUI({ length = 3, bgColor = 'bg-white' }: { length?: number; bgColor?: string }) {
   return (
     <div className='animate-pulse'>
       <ul className='flex flex-col gap-4 w-full'>
-        {Array.from({ length: DivLength }, (v, i) => i).map(el => (
-          <li key={el} className='bg-white rounded-[10px] h-[110px]' />
+        {Array.from({ length }, (_, i) => i).map(el => (
+          <PostListItemSkeletonUI key={el} bgColor={bgColor} />
         ))}
       </ul>
     </div>
   );
 }
 
-export default PostListSkeletonUI;
+function PostListItemSkeletonUI({ bgColor }: { bgColor?: string }) {
+  return <li className={`animate-pulse list-none rounded-[10px] h-[110px] ${bgColor || ''}`} />;
+}
+
+export const PostSkeletonUI = {
+  Container: PostContainerSkeletonUI,
+  List: PostListSkeletonUI,
+  Item: PostListItemSkeletonUI,
+};
